5e0e0569ee2ad9e90aed532aff529d9413de45cf
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2026-06-14 13:04:09 +0200

    docs(#4): javadoc

--- src/main/java/dev/mikablondo/imprint/store/FileImprintStore.java
+++ src/main/java/dev/mikablondo/imprint/store/FileImprintStore.java
@@ -1,13 +1,28 @@
 package dev.mikablondo.imprint.store;
 
+import dev.mikablondo.imprint.Imprint;
 import dev.mikablondo.imprint.ImprintStore;
 
+/**
+ * {@link Imprint} implementation that stores the serialized object in a file,
+ * and returns a short UUID as seed.
+ */
 public class FileImprintStore implements ImprintStore {
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec Stores the binary data in a file and returns a unique key.
+     */
     @Override
     public String save(byte[] data) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec Retrieves the binary data from a file using the provided key.
+     */
     @Override
     public byte[] load(String key) {
         return new byte[0];
