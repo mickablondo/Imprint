b3e788c5d40cc0ab403f12efa027e974597b1d45
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2026-06-16 18:39:26 +0200

    fix(#9): wrong class impl

--- src/main/java/dev/mikablondo/imprint/store/InMemoryImprintStore.java
+++ src/main/java/dev/mikablondo/imprint/store/InMemoryImprintStore.java
@@ -2,28 +2,42 @@ package dev.mikablondo.imprint.store;
 
 import dev.mikablondo.imprint.ImprintStore;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * {@link ImprintStore} implementation that stores the serialized object in memory,
  * and returns a short UUID as seed.
  */
 public class InMemoryImprintStore implements ImprintStore {
-    /**
-     * {@inheritDoc}
-     *
-     * @implSpec Stores the binary data in memory and returns a unique key.
-     */
+
+    // one map by instance, so that we can have multiple instances of the store with different data
+    private final Map<String, byte[]> storage = new ConcurrentHashMap<>();
+
     @Override
     public String save(byte[] data) {
-        return "";
+        String key;
+
+        // create a unique key by generating a random UUID and taking the first 8 characters
+        do {
+            key = UUID.randomUUID()
+                    .toString()
+                    .replace("-", "")
+                    .substring(0, 8);
+        } while (storage.containsKey(key));
+
+        storage.put(key, data);
+        return key;
     }
 
     /**
      * {@inheritDoc}
      *
-     * @implSpec Retrieves the binary data from memory using the provided key.
+     * @implSpec Retrieves the binary data from a database using the provided key.
      */
     @Override
     public byte[] load(String key) {
-        return new byte[0];
+        return storage.get(key);
     }
 }
--- src/main/java/dev/mikablondo/imprint/store/JdbcImprintStore.java
+++ src/main/java/dev/mikablondo/imprint/store/JdbcImprintStore.java
@@ -2,42 +2,28 @@ package dev.mikablondo.imprint.store;
 
 import dev.mikablondo.imprint.ImprintStore;
 
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-
 /**
  * {@link ImprintStore} implementation that stores the serialized object in a database,
  * and returns a short UUID as seed.
  */
 public class JdbcImprintStore implements ImprintStore {
-
-    // one map by instance, so that we can have multiple instances of the store with different data
-    private final Map<String, byte[]> storage = new ConcurrentHashMap<>();
-
+    /**
+     * {@inheritDoc}
+     *
+     * @implSpec Stores the binary data in memory and returns a unique key.
+     */
     @Override
     public String save(byte[] data) {
-        String key;
-
-        // create a unique key by generating a random UUID and taking the first 8 characters
-        do {
-            key = UUID.randomUUID()
-                    .toString()
-                    .replace("-", "")
-                    .substring(0, 8);
-        } while (storage.containsKey(key));
-
-        storage.put(key, data);
-        return key;
+        return "";
     }
 
     /**
      * {@inheritDoc}
      *
-     * @implSpec Retrieves the binary data from a database using the provided key.
+     * @implSpec Retrieves the binary data from memory using the provided key.
      */
     @Override
     public byte[] load(String key) {
-        return storage.get(key);
+        return new byte[0];
     }
 }
